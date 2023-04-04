f70c34fa777602c3c6af284b7cb03d81bdd727ea
SAN_LEN_P14S\sandeep <sandeep.spatil@gmail.com>
2023-04-04 11:54:06 +0530

    Code Status after Unit 1 Week 13 reading

---
+++ app/src/main/java/com/ltu/m7019e/v23/themoviedb/MovieListFragment.kt
@@ -0,0 +1,60 @@
+package com.ltu.m7019e.v23.themoviedb
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.ltu.m7019e.v23.themoviedb.database.Movies
+import com.ltu.m7019e.v23.themoviedb.databinding.FragmentMovieListBinding
+import com.ltu.m7019e.v23.themoviedb.databinding.MovieListItemBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class MovieListFragment : Fragment() {
+
+    private var _binding: FragmentMovieListBinding? = null;
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_movie_list, container, false)
+        val movies = Movies()
+
+        movies.list.forEach { movie ->
+            val movieListItemBinding: MovieListItemBinding = DataBindingUtil.inflate(inflater, R.layout.movie_list_item, container, false);
+            movieListItemBinding.movie = movie
+
+            binding.movieListLl.addView(movieListItemBinding.root)
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        val movies = Movies()
+//
+//        val movieList = view.findViewById<LinearLayout>(R.id.movie_list_ll)
+//        val movieItem = movieList.findViewById<View>(R.id.movie_1)
+//        val movieTitle = movieItem.findViewById<TextView>(R.id.movie_title)
+//        val moviePoster = movieItem.findViewById<ImageView>(R.id.movie_poster)
+//
+//        movieTitle.text = movies.list[0].title
+//        Glide
+//            .with(this)
+//            .load(Contants.POSTER_IMAGE_BASE_URL + Contants.POSTER_IMAGE_WIDTH + movies.list[0].posterPath)
+//            .into(moviePoster);
+
+
+//        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
+    }
+}
---
+++ app/src/main/java/com/ltu/m7019e/v23/themoviedb/model/Movie.kt
@@ -0,0 +1,8 @@
+package com.ltu.m7019e.v23.themoviedb.model
+
+data class Movie(
+        var title: String,
+        var posterPath: String,
+        var releaseDate: String,
+        var overview: String
+)
