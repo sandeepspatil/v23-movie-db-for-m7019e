8aea157ead6aae7e0aa1b61f930bef26703718de
Singhto69 <Timmerskold@hotmail.com>
2023-04-13 14:05:21 +0200

    Update MainActivity.kt

--- app/src/main/java/com/ltu/m7019e/v23/themoviedb/MainActivity.kt
+++ app/src/main/java/com/ltu/m7019e/v23/themoviedb/MainActivity.kt
@@ -10,9 +10,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_second)
-        //setContentView(R.layout.activity_main)
-        //setSupportActionBar(findViewById(R.id.toolbar))
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
